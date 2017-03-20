64add5be6216dee94d0e74e80fe21c0fa7cf7788
Qian Du <duqian91@users.noreply.github.com>
2017-03-20 10:14:50 -0400

    Add static templates

--- guide/urls.py
+++ guide/urls.py
@@ -7,6 +7,19 @@ from guide import API_views
 
 urlpatterns = [
 
+    # Page urls
+    url(r'^$',
+        TemplateView.as_view(template_name="pages/index.html"),
+        name='index',
+        ),
+
+    url(r'^$',
+        TemplateView.as_view(template_name="pages/about.html"),
+        name='about',
+        ),
+    
     # API urls
     url(r'^api/guide/$', API_views.GuideAPIList.as_view()),
+    
+    
 ]
